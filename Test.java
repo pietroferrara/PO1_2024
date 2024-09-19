9189b95c39cdedefaf86a1c7804e7d9af8445205
Pietro Ferrara <pietro.ferrara@unive.it>
2024-09-19 16:30:24 +0200

    first commit

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 15;
+        int z = somma(i, j);
+        int k = i*j/(i*18);
+        System.out.println(k);
+    }
+
+    private static int somma(int i, int j) {
+        return i+j;
+    }
+}
