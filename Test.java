6df0d8c04ba538fb08775713499089cb8e3c31f6
Pietro Ferrara <pietro.ferrara@unive.it>
2024-09-20 14:52:21 +0200

    commit hello world and bytecode

--- Test.java
+++ Test.java
@@ -1,13 +1,27 @@
 public class Test {
     public static void main(String[] args) {
-        int i = 10;
+        System.out.println("Hello World");
+        /*int i = 10;
         int j = 15;
-        int z = somma(i, j);
+        //int z = somma(i, j);
         int k = i*j/(i*18);
-        System.out.println(k);
+        System.out.println(k);*/
     }
 
-    private static int somma(int i, int j) {
-        return i+j;
+
+    private int doppiasomma(int i, int j, int z) {
+        return somma(i, somma(j, z));
+    }
+
+    private int somma(int i, int j) {
+        int z = i + j;
+        int k = 0;
+        return z;
+    }
+
+    private int abs(int i) {
+        if(i < 0)
+            return -i;
+        else return i;
     }
 }
