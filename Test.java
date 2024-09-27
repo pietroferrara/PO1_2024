a1afa866ea12dac117357994a826233d6f2e8abb
Pietro Ferrara <pietro.ferrara@unive.it>
2024-09-27 10:16:21 +0200

    aliasing

--- Test.java
+++ Test.java
@@ -1,9 +1,16 @@
 public class Test {
     public static void main(String[] args) {
+        /* TIPI VALORE
+        int i = 10;
+        int j = i;
+        j = 18;
+        double d = 10.23;
+        char c = 'a';
+        boolean b = true;*/
         Giocatore g1 = new Giocatore();
-        Giocatore g2 = new Giocatore();
         Briscola partita = new Briscola();
-        partita.partita(g1, g2);
+        partita.partita(g1, g1);
+
 
     }
 
